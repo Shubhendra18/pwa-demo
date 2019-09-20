5efe8748b019958f53136e739f5e0b618f622133
Shubhendra Srivastava <himanshusri1808@gmail.com>
2019-09-20 19:51:21 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyCF-LRlQJ1vINlwf5k0Uarf4C1HqNVpHKg",
-    authDomain: "work-3835f.firebaseapp.com",
-    databaseURL: "https://work-3835f.firebaseio.com",
-    projectId: "work-3835f",
-    storageBucket: "",
-    messagingSenderId: "113530084120",
-    appId: "1:113530084120:web:fc01884fb8963706c95c6d"
+    
   }
 };
 
